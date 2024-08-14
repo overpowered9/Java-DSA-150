dfaa95859dd01eb3bea24f30baa1602828ec66bc
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-08-14 18:39:57 +0500

    Create nain.java

---
+++ nain.java
@@ -0,0 +1,11 @@
+
+public class nain {
+
+    public static void main() {
+        int[] nums={1,2,2};
+        Solution sol = new Solution();
+        int num2 = sol.removeDuplicates(nums);
+        System.out.println(num2);
+        
+    }
+}
